df4e43443bf10f10c9e8842a0aee6614c70442b0
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-05-19 16:37:11 -0300

    add: Código do Arduino 1

---
+++ arduinoUm.cpp
@@ -0,0 +1,27 @@
+int btnPedra = 7;
+int btnPapel = 8;
+int btnTesoura = 9;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(btnPedra, INPUT);
+  pinMode(btnPapel, INPUT);
+  pinMode(btnTesoura, INPUT);
+}
+
+void loop() {
+  if (digitalRead(btnPedra) == HIGH) {
+    Serial.println("Pedra");
+    delay(500);  // Debounce
+  }
+
+  if (digitalRead(btnPapel) == HIGH) {
+    Serial.println("Papel");
+    delay(500);
+  }
+
+  if (digitalRead(btnTesoura) == HIGH) {
+    Serial.println("Tesoura");
+    delay(500);
+  }
+}
